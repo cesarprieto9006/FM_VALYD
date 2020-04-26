c20f7557254909f96247164f551a6c4287e98c97
cesarprieto90 <cesarprieto90@gmail.com>
2020-04-26 14:04:06 -0500

    Fusion de la rama feature con la implementacion de portrait y landscape

--- app/src/main/java/com/example/fina/fm_valid/ui/BaseFragment.java
+++
@@ -1,7 +0,0 @@
-package com.example.fina.fm_valid.ui;
-
-import androidx.fragment.app.Fragment;
-
-public abstract class BaseFragment extends Fragment {
-    protected abstract void searchUserName(String userName);
-}
